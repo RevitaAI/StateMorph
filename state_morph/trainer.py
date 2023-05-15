de4c04f5a4e80cd779f52c37b3f01c925f4ac806
jue.hou <jue.hou@helsinki.fi>
2023-05-16 00:48:26 +0300

    apply dask

--- state_morph/trainer.py
+++ state_morph/trainer.py
@@ -1,24 +1,27 @@
 
 from .core import BaseModel
-from .utils import _map_step, _reduce_step
-import random
+from .utils import _map_step, _reduce_step_wrapper, _random_segment, _merge_morph
 from statistics import mean
-from multiprocessing import Pool
+import random
+import dask
+
+
 
 class StateMorphTrainer(object):
-    def __init__(self, num_state, delta=1e-6, patience=10, init_temp=100, final_temp=1e-4, alpha=0.95) -> None:
+    def __init__(self, num_state, delta=1e-6, patience=10, init_temp=100, final_temp=1e-4, alpha=0.95, cluster=None) -> None:
         self.num_state = num_state
-        self.__delta = delta
-        self.__patience = patience
-        self.__final_temp = final_temp
-        self.__alpha = alpha
-        self.__current_temp = init_temp
+        self._delta = delta
+        self._patience = patience
+        self._final_temp = final_temp
+        self._alpha = alpha
+        self._current_temp = init_temp
+        
     
     def load_raw_corpus(self, corpus_file, **kwargs) -> None:
         """Load corpus to state morphology model."""
         with open(corpus_file, 'r', encoding='utf-8') as f:
             corpus = f.read().splitlines()
-            segmented_corpus = self.__random_segment(corpus)
+            segmented_corpus = _random_segment(corpus, self.num_state)
             model_params = {
                 'morph_dict':  {},
                 'state_freq': {},
@@ -28,74 +31,39 @@ class StateMorphTrainer(object):
             }
             model = BaseModel(model_params, **kwargs)
             model.update_segmented_corpus(segmented_corpus)
-            self.__base_model = model
-    
-    def __random_segment(self, corpus) -> list:
-        segmented_corpus = []
-        for word in corpus:
-            segment = []
-            if len(word) > 1:
-                j = 0
-                for i in random.sample(list(range(1, len(word))), random.randint(1, len(word) - 1)):
-                    morph = word[j:i]
-                    if len(morph) >= 1:
-                        segment.append((morph, random.randint(1, self.num_state)))
-                        j = i
-                morph = word[j:]
-                if len(morph) >= 1:
-                    segment.append((morph, random.randint(1, self.num_state)))
-            else:
-                segment.append((word, random.randint(1, self.num_state)))
-            segmented_corpus.append((segment, 0))
-        return segmented_corpus
-             
-    def __merge_morph(self, segmented_corpus) -> list:
-        corpus = []
-        for segment, _ in segmented_corpus:
-            word = ''.join([morph for morph, _ in segment])
-            corpus.append(word)
-        return corpus
-    
+            self._base_model = model
     
     def train(self, iteration=10, num_processes=5) -> BaseModel:
         """Train state morphology model."""
-        model_param = self.__base_model.get_param_dict()
-        segmented_corpus = self.__base_model.segmented_corpus
+        model_param = self._base_model.get_param_dict()
+        segmented_corpus = self._base_model.segmented_corpus
         p_loss = -1
         count = 0
         for _ in range(iteration):
-            print('Iteration:', _, 'Temperature:', self.__current_temp)
+            print('Iteration:', _, 'Temperature:', self._current_temp)
             partition_size = len(segmented_corpus) // num_processes
             partitions = [segmented_corpus[i:i+partition_size]
                          for i in range(0, len(segmented_corpus), partition_size)]
             temp = partitions[:-1]
             temp[-1] += partitions[-1]
             partitions = temp 
-            partition = [(_, model_param, partition) for _, partition in enumerate(partitions)]
+            partitions = [(_, model_param, partition) for _, partition in enumerate(partitions)]
             
-            total_model_param = {
-                'morph_dict': {},
-                'state_freq': {},
-                'state_size': {},
-                'state_char_counts': {},
-                'transition_freq': [],
-            }
-            total_segmented_corpus = []
-            with Pool(num_processes) as p:
-                for model_param, segmented_corpus in p.starmap(_map_step, partition):
-                    _reduce_step(total_model_param, total_segmented_corpus, model_param, segmented_corpus)
-                p.close()
-                p.join()
+            with dask.config.set(num_workers=num_processes):
+                map_outputs = []
+                for partition in partitions:
+                    delayed_map_step = dask.delayed(_map_step)(*partition)
+                    map_outputs.append(delayed_map_step)
+                delayed_reduce_step = dask.delayed(_reduce_step_wrapper)(map_outputs)
+                model_param, segmented_corpus = delayed_reduce_step.compute()
             print('Reduce step finished...')
-            model_param = total_model_param
-            segmented_corpus = total_segmented_corpus
             loss = mean([cost for _, cost in segmented_corpus if cost > 0])
             print('Iteration: {}, Cost: {}'.format(_, loss))
             
             # Early stopping
-            if abs(p_loss - loss) < self.__delta:
+            if abs(p_loss - loss) < self._delta:
                 count += 1
-                if count == self.__patience:
+                if count == self._patience:
                     print('Early stopping...')
                     break
             else:
@@ -103,9 +71,10 @@ class StateMorphTrainer(object):
                 p_loss = loss
                 
             random.shuffle(segmented_corpus)
-            i = int(len(segmented_corpus) * self.__current_temp)
-            segmented_corpus = self.__random_segment(self.__merge_morph(segmented_corpus[:i])) + segmented_corpus[i:]
-            self.__current_temp = max(self.__final_temp, self.__current_temp * self.__alpha)
+            i = int(len(segmented_corpus) * self._current_temp)
+            segmented_corpus = _random_segment(_merge_morph(segmented_corpus[:i]), self.num_state) + \
+                segmented_corpus[i:]
+            self._current_temp = max(self._final_temp, self._current_temp * self._alpha)
             
         new_model = BaseModel(model_param)
         new_model.update_segmented_corpus(segmented_corpus, update_model=False)
--- state_morph/utils.py
+++ state_morph/utils.py
@@ -1,4 +1,5 @@
 from .core import BaseModel
+import random
 
 def _map_step(partition_id, model_param, segmented_corpus):
     """Map step function for multiprocessing."""
@@ -20,26 +21,66 @@ def _reduce_step(total_model_param, total_corpus, model_param, segmented_corpus)
         for vk, vv in v.items():            
             if vk not in total_model_param['morph_dict'][k]:
                 total_model_param['morph_dict'][k][vk] = 0
-            total_model_param['morph_dict'][k][vk] += vv
+            total_model_param['morph_dict'][k][vk] = total_model_param['morph_dict'][k][vk] + vv
     for k, v in model_param['state_freq'].items():
         if k not in total_model_param['state_freq']:
             total_model_param['state_freq'][k] = 0
-        total_model_param['state_freq'][k] += v
+        total_model_param['state_freq'][k] = total_model_param['state_freq'][k] + v
     for k, v in model_param['state_size'].items():
         if k not in total_model_param['state_size']:
             total_model_param['state_size'][k] = 0
-        total_model_param['state_size'][k] += v
+        total_model_param['state_size'][k] = total_model_param['state_size'][k] + v
     for k, v in model_param['state_char_counts'].items():
         if k not in total_model_param['state_char_counts']:
             total_model_param['state_char_counts'][k] = {}
         for vk, vv in v.items():
             if vk not in total_model_param['state_char_counts'][k]:
                 total_model_param['state_char_counts'][k][vk] = 0
-            total_model_param['state_char_counts'][k][vk] += vv
+            total_model_param['state_char_counts'][k][vk] = total_model_param['state_char_counts'][k][vk] + vv
             
     if not len(total_model_param['transition_freq']):
         total_model_param['transition_freq'] = model_param['transition_freq']
     else:
         for i in range(len(model_param['transition_freq'])):
             for j in range(len(model_param['transition_freq'][i])):
-                total_model_param['transition_freq'][i][j] += model_param['transition_freq'][i][j]
+                total_model_param['transition_freq'][i][j] = total_model_param['transition_freq'][i][j] + \
+                    model_param['transition_freq'][i][j]
+                
+def _reduce_step_wrapper(map_outputs):
+    total_model_param = {
+        'morph_dict': {},
+        'state_freq': {},
+        'state_size': {},
+        'state_char_counts': {},
+        'transition_freq': [],
+    }
+    total_segmented_corpus = []
+    for model_param, segmented_corpus in map_outputs:
+        _reduce_step(total_model_param, total_segmented_corpus, model_param, segmented_corpus)
+    return total_model_param, total_segmented_corpus                
+          
+def _random_segment(corpus, num_state) -> list:
+        segmented_corpus = []
+        for word in corpus:
+            segment = []
+            if len(word) > 1:
+                j = 0
+                for i in random.sample(list(range(1, len(word))), random.randint(1, len(word) - 1)):
+                    morph = word[j:i]
+                    if len(morph) >= 1:
+                        segment.append((morph, random.randint(1, num_state)))
+                        j = i
+                morph = word[j:]
+                if len(morph) >= 1:
+                    segment.append((morph, random.randint(1, num_state)))
+            else:
+                segment.append((word, random.randint(1, num_state)))
+            segmented_corpus.append((segment, 0))
+        return segmented_corpus
+             
+def _merge_morph(segmented_corpus) -> list:
+    corpus = []
+    for segment, _ in segmented_corpus:
+        word = ''.join([morph for morph, _ in segment])
+        corpus.append(word)
+    return corpus
