e088c130e91c2bb5ec1b63623452f2700af98a1d
jue.hou <jue.hou@helsinki.fi>
2024-03-27 17:32:31 +0200

    minor fix

--- state_morph/__init__.py
+++ state_morph/__init__.py
@@ -1,6 +1,12 @@
 __all__ = ['StateMorphTrainer', 'StateMorphIO', 'BaseModel', 'get_version']
-from ._version import version, __version__
 
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("StateMorph")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = 'Unknown'
 __author__ = 'Jue Hou'
 __author_email__ = "revita@cs.helsinki.fi"
 
